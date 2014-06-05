600d93259c7669f2e0b6c3f1cf6e846976fe6e92
zzzinwoo <zzzinwoo@hongfamily.com>
2014-06-05 15:39:23 +0900

    first edit from github app
    right?

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 public class helloworld{
 	public static void main(String [] args) {
-		System.out.println("hello world!");
+		System.out.print("hello ");
+		System.out.print("world!");
 		System.exit(0);
 	}
 }
